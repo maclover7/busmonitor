afa08a35c218e52222c2d077e09c2b720629f214
Jon Moss <me@jonathanmoss.me>
2021-08-10 00:43:21 -0400

    Add vehicle positions scraper

---
+++ scrape-vehicle.py
@@ -0,0 +1,26 @@
+import csv
+from google.transit import gtfs_realtime_pb2
+import requests
+
+feed = gtfs_realtime_pb2.FeedMessage()
+response = requests.get("https://truetime.portauthority.org/gtfsrt-bus/vehicles")
+feed.ParseFromString(response.content)
+
+vehicles = []
+
+for entity in feed.entity:
+    # Ensure that the vehicle is on a trip and not parked in a garage
+    if entity.vehicle.trip.trip_id:
+        vehicles.append([
+                entity.vehicle.timestamp,
+                entity.vehicle.trip.trip_id,
+                entity.vehicle.trip.route_id,
+                entity.vehicle.trip.schedule_relationship,
+                entity.vehicle.vehicle.id,
+                entity.vehicle.position.latitude,
+                entity.vehicle.position.longitude
+                ])
+
+with open('bus-vehicle.csv', 'a', newline='') as csvfile:
+    csv_file = csv.writer(csvfile, lineterminator='\n')
+    csv_file.writerows(vehicles)
